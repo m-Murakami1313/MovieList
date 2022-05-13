8c7732454885c4a59114e04ea3b0dedace71508d
m-Murakami1313 <mituvv@gmail.com>
2022-05-13 13:55:00 +0900

    add 画像をclickした際に表示される詳細ページを作成した

--- src/pages/MovieDetail/MovieDetail.tsx
+++ src/pages/MovieDetail/MovieDetail.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import { Box } from "@mui/system";
+import { useLocation } from "react-router-dom";
+
+import styles from "./MovieDetail.module.scss";
 
 export const MovieDetail = () => {
+  const location = useLocation();
+  const [result, setResult] = useState(location.state as { headerMovie: any });
+
   return (
-    <div>
-      <Box>
-        <h1>title</h1>
-        <p>公開年</p>
-        <p>あらすじ</p>
+    <div className={styles.container}>
+      <Box
+        className={styles.box}
+        style={{
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundImage: `url("https://image.tmdb.org/t/p/original${result.headerMovie?.backdrop_path}")`,
+        }}
+      >
+        <h1 className={styles.title}>{result.headerMovie.title} </h1>
+        <p className={styles.date}>{result.headerMovie.release_date}</p>
+        <p className={styles.overview}>
+          vote_average {result.headerMovie.vote_average}
+        </p>
+        <p className={styles.overview}>{result.headerMovie.overview}</p>
       </Box>
     </div>
   );
