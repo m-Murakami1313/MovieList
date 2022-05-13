142b72c191e897c565a2ee57f28f2b804bfbf8ac
m-Murakami1313 <mituvv@gmail.com>
2022-05-13 10:12:39 +0900

    add 映画の詳細のページを仮で作成した

---
+++ src/pages/MovieDetail/MovieDetail.tsx
@@ -0,0 +1,13 @@
+import { Box } from "@mui/system";
+
+export const MovieDetail = () => {
+  return (
+    <div>
+      <Box>
+        <h1>title</h1>
+        <p>公開年</p>
+        <p>あらすじ</p>
+      </Box>
+    </div>
+  );
+};
